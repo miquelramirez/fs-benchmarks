1697b408ebe82ca216ae7337cf4737897ba733fa
Guillem Frances <guillem.frances@gmail.com>
2015-10-01 09:39:19 +0200

    Use the new default extensional gecode translators

--- benchmarks/gardening-fn/external.cxx
+++ benchmarks/gardening-fn/external.cxx
@@ -4,4 +4,6 @@
 
 void External::registerComponents() const {
 	LogicalComponentRegistry::instance().addFormulaCreator("@can_move", [](const std::vector<fs::Term::cptr>& subterms){ return new CanMoveFormula(subterms); });
+	// Explicitly register an extensional translator that will be used with gecode
+	LogicalComponentRegistry::instance().add(typeid(CanMoveFormula), new gecode::ExtensionalTranslator("@can_move"));
 }
