7f64124d55ab8b636ebe0c3e1e51a95a56ee63d3
mramirez <miquel.ramirez@nicta.com.au>
2015-10-15 14:42:20 +1100

    Removed '-' from identifiers

---
+++ benchmarks/tpp-num-fn/IPC-5/convert_identifiers.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import sys, os, glob
+
+def normalize_identifiers( text ) :
+
+    stuff_to_subst = [  ('drive-cost', 'drive_cost'),
+                        ('total-cost', 'total_cost'),
+                        ('on-sale', 'on_sale'),
+                        ('TPP-Metric', 'TPP_Metric')
+    ]
+
+    for term, replacement in stuff_to_subst :
+        text = text.replace( term, replacement )
+
+    return text
+
+
+def main( ) :
+
+    pddl_files = glob.glob( '*.pddl' )
+    for filename in pddl_files :
+        lines = []
+        with open( filename, 'r' ) as instream :
+            for line in instream :
+                line = line.strip()
+                lines.append(normalize_identifiers(line))
+
+        with open( filename, 'w') as outstream :
+            output = '\n'.join(lines)
+            outstream.write(output)
+
+if __name__ == '__main__' :
+    main()
